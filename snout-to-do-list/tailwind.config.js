c1670f5551ccdcfad009c8eb8dcb5d8a764af165
oliwilliams1 <imhungryoliver@gmail.com>
2024-10-17 23:54:08 +1300

    Create demo html & css, ready for backend

--- snout-to-do-list/tailwind.config.js
+++ snout-to-do-list/tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
     "./src/**/*.{html,js}", // Adjust paths as necessary
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'snout-base': '#3c424a',
+        'snout-deep': '#343a40',
+        'snout-light': '#ffffff',
+        'snout-bright': '#d9d9d9',
+      },
+    },
   },
   plugins: [],
 };
